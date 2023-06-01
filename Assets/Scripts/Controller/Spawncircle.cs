402576e5d734244140e3859efce56c3f3870e8d9
30dejaneiro <iam30dejaneiro@gmail.com>
2023-06-01 18:54:48 +0700

    fix bug

---
+++ Assets/Scripts/Controller/Spawncircle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawncircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject objectPrefab;
+    public float spawnInterval = 10f;
+
+    private float screenWidth;
+    private float screenHeight;
+    private float timer;
+
+    private void Start()
+    {
+        // Get the screen dimensions
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+
+        // Start the timer
+        timer = spawnInterval;
+    }
+
+    private void Update()
+    {
+        // Update the timer
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            // Reset the timer
+            timer = spawnInterval;
+
+            // Spawn object at a random position
+            Vector3 randomPosition = new Vector3(Random.Range(0, screenWidth), Random.Range(0, screenHeight), +1);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(randomPosition);
+            Instantiate(objectPrefab, worldPosition, Quaternion.identity);
+        }
+    }
+
+}
